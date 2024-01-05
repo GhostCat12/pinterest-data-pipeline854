db119da94e3f890ac52d4337c449a310e3208b1e
GhostCat12 <maryam.hashmi@hotmail.co.uk>
2024-01-05 22:41:51 +0000

    Adds docstrings to user_posting_emulation_streaming.py

--- user_posting_emulation_streaming.py
+++ user_posting_emulation_streaming.py
@@ -12,7 +12,16 @@ headers = {'Content-Type': 'application/json'}
 
 @run_infinite_post_data_loop
 def kinesis_stream_post(pin_result, geo_result, user_result):
-    
+    """Post data to Kinesis streams using the specified API endpoints.
+
+    Args:
+        pin_result (dict): Dictionary containing Pinterest data.
+        geo_result (dict): Dictionary containing geolocation data.
+        user_result (dict): Dictionary containing user data.
+
+    Returns:
+        None
+    """
     payload_pin_result = api_send_to_kinesis(invoke_url_pin_result, headers, table_dict=pin_result, partition_key="pin")
     payload_geo_result = api_send_to_kinesis(invoke_url_geo_result, headers, table_dict=geo_result, partition_key="geo")
     payload_user_result = api_send_to_kinesis(invoke_url_user_result, headers, table_dict=user_result, partition_key="user")
