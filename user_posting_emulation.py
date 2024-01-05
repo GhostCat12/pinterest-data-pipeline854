b5da803ebe5e377eb2235351c5950359a78279eb
GhostCat12 <maryam.hashmi@hotmail.co.uk>
2024-01-05 22:36:27 +0000

    Adds docstrings to user_posting_emulation.py

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -14,9 +14,20 @@ random.seed(100)
 
 
 class AWSDBConnector:
+    """Class for connecting to an AWS RDS database.
 
+    Attributes:
+        HOST (str): The host address of the RDS instance.
+        USER (str): The username for connecting to the database.
+        PASSWORD (str): The password for connecting to the database.
+        DATABASE (str): The name of the database.
+        PORT (int): The port number for the database connection.
+
+    Methods:
+        create_db_connector: Creates and returns a SQLAlchemy engine for the database connection.
+    """
     def __init__(self):
-
+        """Initialize AWSDBConnector with default values."""
         self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
         self.USER = 'project_user'
         self.PASSWORD = ':t%;yCY3Yjg'
@@ -24,6 +35,11 @@ class AWSDBConnector:
         self.PORT = 3306
         
     def create_db_connector(self):
+        """Create and return a SQLAlchemy engine for the database connection.
+
+        Returns:
+            sqlalchemy.engine.base.Engine: SQLAlchemy engine for the database connection.
+        """
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
 
@@ -32,25 +48,51 @@ new_connector = AWSDBConnector()
 
 
 def map_select_row(connection, random_row, table):
-	select_row_string = text(f"SELECT * FROM {table} LIMIT {random_row}, 1")
-	table_selected_row = connection.execute(select_row_string)
+    """Select a random row from the specified table in the database.
+
+    Args:
+        connection (sqlalchemy.engine.base.Connection): SQLAlchemy database connection.
+        random_row (int): Randomly selected row number.
+        table (str): Name of the table to select the row from.
+
+    Returns:
+        dict: Dictionary containing the selected row's data.
+    """
+    select_row_string = text(f"SELECT * FROM {table} LIMIT {random_row}, 1")
+    table_selected_row = connection.execute(select_row_string)
 	
-	for row in table_selected_row: 
-		result_dict = dict(row._mapping)
+    for row in table_selected_row: 
+        result_dict = dict(row._mapping)
 		
-	return result_dict
+    return result_dict
 
 
 def convert_datetime(dict_result):
-	for key, value in dict_result.items():
-		if type(value) == datetime.datetime:
-			dict_result[key] = value.strftime("%Y-%m-%d %H:%M:%S")
+    """Convert datetime values in the dictionary to formatted strings.
 
-	return dict_result
+    Args:
+        dict_result (dict): Dictionary containing data with datetime values.
+
+    Returns:
+        dict: Dictionary with datetime values formatted as strings.
+    """
+    for key, value in dict_result.items():
+        if type(value) == datetime.datetime:
+            dict_result[key] = value.strftime("%Y-%m-%d %H:%M:%S")
+
+    return dict_result
 
 
 
 def run_infinite_post_data_loop(func):
+    """Decorator function for running an infinite loop to post data.
+
+    Args:
+        func: The function to be wrapped.
+
+    Returns:
+        function: Wrapper function for running an infinite loop and invoking the specified function.
+    """
     def wrapper(*args):
         while True:
             sleep(random.randrange(0, 2))
@@ -74,12 +116,22 @@ def run_infinite_post_data_loop(func):
 
 
 def api_send_to_kafka(invoke_url, header, table_dict): 
+    """Send data to Kafka using the specified API.
+
+    Args:
+        invoke_url (str): URL for invoking the API.
+        header (dict): HTTP headers for the API request.
+        table_dict (dict): Dictionary containing data to be sent to Kafka.
+
+    Returns:
+        None
+    """
       
 	#To send JSON messages of table_dict
     payload_json = json.dumps({
             "records": [
                 {
-                #Data should be sent as pairs of column_name:value, with different columns separated by commas        
+                # Data should be sent as pairs of column_name:value, with different columns separated by commas        
                 "value": table_dict
                 }
                 ]
@@ -91,8 +143,18 @@ def api_send_to_kafka(invoke_url, header, table_dict):
 
 
 def api_send_to_kinesis(invoke_url, header, table_dict, partition_key):
-   
-    #To send JSON messages you need to follow this structure
+    """Send data to Kinesis using the specified API.
+
+        Args:
+            invoke_url (str): URL for invoking the API.
+            header (dict): HTTP headers for the API request.
+            table_dict (dict): Dictionary containing data to be sent to Kinesis.
+            partition_key (str): Partition key for Kinesis stream.
+
+        Returns:
+            None
+        """   
+    # To send JSON messages you need to follow this structure
     payload_json = json.dumps({
         "StreamName": f"streaming-0a60b9a8a831-{partition_key}",
         "Data": table_dict,
